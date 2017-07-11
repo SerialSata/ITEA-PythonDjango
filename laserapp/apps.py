dc986eca7b6a436e61e38b70b053c70c1cc005c3
sata <v.romanov.san@gmail.com>
2017-07-11 16:10:16 +0300

    First commit

---
+++ laserapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class LaserappConfig(AppConfig):
+    name = 'laserapp'
---
+++ laserflask/laserflask/my_settings.py
@@ -0,0 +1,10 @@
+from .settings import *
+
+TIME_ZONE = 'Europe/Kiev'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db1.sqlite3'),
+    }
+}
